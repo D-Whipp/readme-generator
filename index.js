d045b47e6758c1704d22c92e8fd1cb5348e3bd9a
Mr Whipp <dwhipp88@gmail.com>
2021-08-08 21:39:53 -0600

    added questions

--- index.js
+++ index.js
@@ -1,7 +1,129 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const promptQuestions = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name? (Required)",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your name!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Enter your GitHub Username: (Required)",
+      validate: (githubInput) => {
+        if (githubInput) {
+          return true;
+        } else {
+          console.log("Please enter your GitHub Username!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "title",
+      message: "Enter your Project Title: (Required)",
+      validate: (titleInput) => {
+        if (titleInput) {
+          return true;
+        } else {
+          console.log("Please enter your Project Title!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Enter a description: (Required)",
+      validate: (descriptionInput) => {
+        if (descriptionInput) {
+          return true;
+        } else {
+          console.log("Please enter your Project's Description!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "How is your project installed? (Required)",
+      validate: (installInput) => {
+        if (installInput) {
+          return true;
+        } else {
+          console.log("Please explain how your project is installed!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "How is your project used? (Required)",
+      validate: (usageInput) => {
+        if (usageInput) {
+          return true;
+        } else {
+          console.log("Explain how your project is used: (Required)");
+          return false;
+        }
+      },
+    },
+    {
+      type: "confirm",
+      name: "confirmCredits",
+      message: "Are there Credits you'd like to include?",
+      default: true,
+    },
+    {
+      type: "input",
+      name: "credits",
+      message: "Include credits here:",
+      when: ({ confirmCredits }) => confirmCredits,
+    },
+    {
+      type: "confirm",
+      name: "confirmLicense",
+      message: "Would you like to include a license?",
+      default: false,
+    },
+    {
+      type: "input",
+      name: "license",
+      message: "Include license(s) here:",
+      when: ({ confirmLicense }) => confirmLicense,
+    },
+    {
+      type: "confirm",
+      name: "confirmBadge",
+      message: "Would you like to include a badge?",
+      default: false,
+    },
+    {
+      type: "input",
+      name: "badge",
+      message: "Include badge(s) here:",
+      when: ({ confirmBadge }) => confirmBadge,
+    },
+  ]);
+};
+
+promptQuestions();
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
